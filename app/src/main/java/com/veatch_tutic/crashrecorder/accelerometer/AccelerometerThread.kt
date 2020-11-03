43c64d8c649c41adcd0953f77e39501b57d935bc
Jacob Veatch <jakedveatch@gmail.com>
2020-11-02 20:08:23 -0600

    add accel reading and crash detection on threas

--- app/src/main/java/com/veatch_tutic/crashrecorder/accelerometer/AccelerometerThread.kt
+++ app/src/main/java/com/veatch_tutic/crashrecorder/accelerometer/AccelerometerThread.kt
@@ -1,15 +1,61 @@
 package com.veatch_tutic.crashrecorder.accelerometer
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Handler
 import com.veatch_tutic.crashrecorder.video_streaming.VideoStreamingThread
+import kotlin.math.abs
 
-class AccelerometerThread(private val messageHandler: Handler) : Thread() {
+class AccelerometerThread(
+    private val messageHandler: Handler,
+    private val sensorManager: SensorManager
+) : Thread(), SensorEventListener {
+    val CRASH_DETECTION_THRESHOLD = 5.0
 
     override fun run() {
         System.out.println("Accelerometer Thread running")
-        // run and monitor the accelerometer thread
 
-        // on trigger of crash detection, pass message to messageHandler
-        messageHandler.sendEmptyMessage(VideoStreamingThread.MESSAGE_CODE)
+        val accelerometer: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        // run and monitor the accelerometer thread
+        accelerometer?.let {
+            // register this thread as listener
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun destroy() {
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        System.out.println("sensor changed")
+
+        val gravity = floatArrayOf()
+        val linearAcceleration = floatArrayOf()
+
+        val alpha = 0.8f
+
+        // Isolate the force of gravity with the low-pass filter.
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0]
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1]
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2]
+
+        // Remove the gravity contribution with the high-pass filter.
+        linearAcceleration[0] = event.values[0] - gravity[0]
+        linearAcceleration[1] = event.values[1] - gravity[1]
+        linearAcceleration[2] = event.values[2] - gravity[2]
+
+        val accelerationForce = abs(linearAcceleration[0]) + abs(linearAcceleration[1]) + abs(linearAcceleration[2])
+
+        if (accelerationForce >= CRASH_DETECTION_THRESHOLD) {
+            messageHandler.sendEmptyMessage(VideoStreamingThread.MESSAGE_CODE)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // no-op
     }
 }
